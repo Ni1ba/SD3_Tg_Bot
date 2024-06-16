731cdfbb576fd07ec478461abbc6bd8652a7180d
Ni1ba <danila.baldin@mail.ru>
2024-06-16 20:21:45 +0300

    простая клавиатурая, пока не используется

---
+++ KeyBoards.cs
@@ -0,0 +1,22 @@
+﻿using Telegram.Bot.Types.ReplyMarkups;
+
+namespace SD3_Tg_Bot
+{
+    internal class KeyBoards
+    {
+        public ReplyKeyboardMarkup MainMenuKeyBoard()
+        {
+            ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
+            {
+                new KeyboardButton[] { "Help me", "Call me ☎️" },
+            })
+            {
+                ResizeKeyboard = true
+            };
+
+
+            return replyKeyboardMarkup;
+        }
+
+    }
+}
