868ea13420bd02944cbd0c2bf0e0347f81677115
Ni1ba <danila.baldin@mail.ru>
2024-06-16 23:56:53 +0300

    тип у сида поменян

--- GenerateSettings.cs
+++ GenerateSettings.cs
@@ -4,7 +4,7 @@
     {
         public string prompt { get; set; }
         public string negativePrompt { get; set; }
-        public string seed { get; set; }
+        public int seed { get; set; }
         public bool randomizeSeed { get; set; }
         public int width { get; set; }
         public int height { get; set; }
