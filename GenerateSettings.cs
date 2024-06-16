47a2729c9e09744af98f11e6fea6e8df8fa80106
Ni1ba <danila.baldin@mail.ru>
2024-06-16 23:55:12 +0300

    класс с полями для генерации изображений, с настройками по умолчанию

---
+++ GenerateSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace SD3_Tg_Bot
+{
+    internal class GenerateSettings
+    {
+        public string prompt { get; set; }
+        public string negativePrompt { get; set; }
+        public string seed { get; set; }
+        public bool randomizeSeed { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+        public double guidanceScale { get; set; }
+        public int numInferenceSteps { get; set; }
+
+
+        public GenerateSettings() { }
+
+    }
+}
