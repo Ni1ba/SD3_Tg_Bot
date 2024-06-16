1b58817433a1b5985e23bf01696a1f8921300f14
Ni1ba <danila.baldin@mail.ru>
2024-06-17 00:01:38 +0300

    Перенес параметры генерации в новый класс

--- GenerateSettings.cs
+++ GenerateSettings.cs
@@ -1,7 +1,13 @@
-﻿namespace SD3_Tg_Bot
+﻿using Newtonsoft.Json;
+using System.Text;
+using Newtonsoft.Json;
+using static System.Console;
+
+namespace SD3_Tg_Bot
 {
     internal class GenerateSettings
     {
+        private static readonly HttpClient client = new HttpClient();
         public string prompt { get; set; }
         public string negativePrompt { get; set; }
         public int seed { get; set; }
@@ -12,7 +18,41 @@
         public int numInferenceSteps { get; set; }
 
 
-        public GenerateSettings() { }
+        public GenerateSettings() 
+        { 
+        
+        }
 
+        public static async Task<string> GenerateByPrompt()
+        {
+            var url = "http://127.0.0.1:5000/predict"; // Убедитесь, что URL правильный
+            var data = new
+            {
+                prompt = "Hello!!",
+                negative_prompt = "Hello!!",
+                seed = 0,
+                randomize_seed = true,
+                width = 1024,
+                height = 1024,
+                guidance_scale = 5,
+                num_inference_steps = 28
+            };
+
+            var response = await CallApi(url, data);
+            WriteLine(response);
+            return null;
+        }
+
+        public static async Task<string> CallApi(string url, object data)
+        {
+            var json = JsonConvert.SerializeObject(data);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync(url, content);
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            return responseString;
+        }
     }
 }
