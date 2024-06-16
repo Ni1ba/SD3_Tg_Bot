4671aa7ac7945c2bdfa2cfef09a7415a8fef705e
Ni1ba <danila.baldin@mail.ru>
2024-06-16 23:51:09 +0300

    клавиатура главного меню

--- KeyBoards.cs
+++ KeyBoards.cs
@@ -4,7 +4,7 @@ namespace SD3_Tg_Bot
 {
     internal class KeyBoards
     {
-        public ReplyKeyboardMarkup MainMenuKeyBoard()
+        public ReplyKeyboardMarkup ReplyMainMenuKeyBoard()
         {
             ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
             {
@@ -16,6 +16,27 @@ namespace SD3_Tg_Bot
 
 
             return replyKeyboardMarkup;
+        }
+
+        public InlineKeyboardMarkup InlineMainMenuKeyBoard()
+        {
+            InlineKeyboardMarkup inlineKeyboard = new(new[]
+             {
+                // first row
+                new []
+                {
+                    InlineKeyboardButton.WithCallbackData(text: "Промпт", callbackData: "prompt"),
+                    InlineKeyboardButton.WithCallbackData(text: "Негативный промпт", callbackData: "negativePrompt"),
+                },
+                // second row
+                new []
+                {
+                    InlineKeyboardButton.WithCallbackData(text: "Генерация", callbackData: "generate"),
+                    //InlineKeyboardButton.WithCallbackData(text: "2.2", callbackData: "22"),
+                },
+            });
+
+            return inlineKeyboard;
         }
 
     }
